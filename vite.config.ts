083758b9b2741a9ff26b3b70130b9db14763818e
lennox-254 <l3nnoxkipruto@gmail.com>
2026-05-11 22:28:52 +0300

    chore: push missing vercel and vite configurations

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
-    tanstackStart(),
+    TanStackRouterVite(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
